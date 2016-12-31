a743c88b6fcbc3827b592893193b2e1ca925b98f
drobinso <drobinso@adobe.com>
2016-12-30 22:17:44 -0800

    updating test assertion

--- test/lightbox_test.js
+++ test/lightbox_test.js
@@ -24,7 +24,7 @@ describe('LightBox instance', function(){
         expect(lightbox.__proto__.hasOwnProperty('update')).to.be.ok;
     });
 
-    it('template should be rendered with .lightbox class attribute and assigned to lightbox.elm property',function(){
+    it('should have template containing "lightbox" as first class in classList ',function(){
         var obj = {
             node: document.createElement('div'),
             model : []
