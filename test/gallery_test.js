9b54d84f8e05782eb7b2e2b75e3f5081a3647a35
drobinso <drobinso@adobe.com>
2017-01-02 00:07:22 -0800

    adding before, beforeEach to improve test suites readability

--- test/gallery_test.js
+++ test/gallery_test.js
@@ -4,109 +4,87 @@ var jsdom = require('jsdom');
 global.DOMParser = jsdom.jsdom().defaultView.DOMParser;
 
 describe('Gallery instance', function(){
-
     var Gallery = require('../app/js/gallery/gallery');
-    var CONSTRUCTOR_ERROR = /Gallery Requires DOM Node/;
+    var CONSTRUCTOR_ERROR, instance, goodConstructor, badConstructor, fnBad, fnGood;
 
-    it('constructor should throw error if option arg does not have DOM node',function(){
+    before(function(){
+        CONSTRUCTOR_ERROR = /Gallery Requires DOM Node/;
 
-        var badConstructor = function() {
-            return new Gallery({
-                node: '#type-error'
-            });
-        };
+        instance = new Gallery({
+            node: document.createElement('div')
+        });
 
-        var goodConstructor = function() {
+        goodConstructor = function() {
             return new Gallery({
                 node: document.createElement('div')
             });
         };
 
-        var fnBad = function(){new badConstructor()};
-        var fnGood = function(){new goodConstructor()};
+        badConstructor = function() {
+            return new Gallery({
+                node: '#type-error'
+            });
+        };
 
+        fnGood = function(){new goodConstructor()};
+        fnBad = function(){new badConstructor()};
+    });
+
+    it('constructor should throw error if option arg does not have DOM node',function(){
         expect(fnBad).to.throw(CONSTRUCTOR_ERROR);
         expect(fnGood).not.to.throw(CONSTRUCTOR_ERROR);
     });
 
     it('should contain renderUI, fetchData, addFigure, resolver, and catcher methods',function(){
-
-        var gallery = new Gallery({
-            node: document.createElement('div')
-        });
-
-        expect(gallery).to.have.property('renderUI');
-        expect(gallery).to.have.property('fetchData');
-        expect(gallery).to.have.property('addFigure');
-        expect(gallery).to.have.property('resolver');
-        expect(gallery).to.have.property('catcher');
+        expect(instance).to.have.property('renderUI');
+        expect(instance).to.have.property('fetchData');
+        expect(instance).to.have.property('addFigure');
+        expect(instance).to.have.property('resolver');
+        expect(instance).to.have.property('catcher');
     });
 
 });
 
 
 describe('Gallery behavior', function(){
-
     var Gallery = require('../app/js/gallery/gallery');
-    var ADD_FIGURE_ERROR = /Gallery addFigure method requires src string arg/;
+    var ADD_FIGURE_ERROR, instance, searchField, imagesContainer;
 
-    it('renderUI method should add "container" to gallery container classList', function(){
+    before(function(){
+        ADD_FIGURE_ERROR = /Gallery addFigure method requires src string arg/;
+    });
 
-        var gallery = new Gallery({
+    beforeEach(function(){
+        instance = new Gallery({
             node: document.createElement('div')
         });
 
-        expect(gallery.container.classList[0]).to.equal('container');
+        searchField = instance.container.querySelector('.search-term');
+        imagesContainer = instance.container.querySelector('.images');
+    });
+
+    it('renderUI method should add "container" to gallery container classList', function(){
+        expect(instance.container.classList[0]).to.equal('container');
     });
 
     it('renderUI method should render template .search-term and .images selectors',function(){
-
-        var gallery = new Gallery({
-            node: document.createElement('div')
-        });
-
-        var searchField = gallery.container.querySelector('.search-term');
-        var imagesContainer = gallery.container.querySelector('.images');
-
         expect(searchField.classList[0]).to.equal('search-term');
         expect(imagesContainer.classList[0]).to.equal('images');
     });
 
     it('renderUI method should register keyup event handler on search field', function(){
-
-        var gallery = new Gallery({
-            node: document.createElement('div')
-        });
-
-        var searchField = gallery.container.querySelector('.search-term');
-
         expect(typeof searchField.onkeyup).to.equal('function');
     });
 
     it('addFigure method should throw error if src arg is not a String', function(){
-
-        var gallery = new Gallery({
-            node: document.createElement('div')
-        });
-
-        var fnBad = function(){gallery.addFigure([])};
-        var fnGood = function(){gallery.addFigure('https://c2.staticflickr.com/6/5574/30167563922_684e8d6796_b.jpg')};
-
+        var fnBad = function(){instance.addFigure([])};
+        var fnGood = function(){instance.addFigure('')};
         expect(fnBad).to.throw(ADD_FIGURE_ERROR);
         expect(fnGood).not.to.throw(ADD_FIGURE_ERROR);
     });
 
     it('addFigure method should increase childNodes count by one', function(){
-
-        var gallery = new Gallery({
-            node: document.createElement('div')
-        });
-
-        gallery.addFigure('https://c2.staticflickr.com/6/5574/30167563922_684e8d6796_b.jpg');
-
-        var imagesContainer = gallery.container.querySelector('.images');
-
+        instance.addFigure('');
         expect(imagesContainer.childNodes.length).to.equal(1);
     });
-    
 });
--- test/lightbox_test.js
+++ test/lightbox_test.js
@@ -4,66 +4,52 @@ var jsdom = require('jsdom');
 global.DOMParser = jsdom.jsdom().defaultView.DOMParser;
 
 describe('LightBox instance', function(){
-
     var LightBox = require('../app/js/gallery/lightbox');
+    var CONSTRUCTOR_ERROR, instance, goodConstructor, badConstructor, fnGood, fnBad, closeBtn, mainImg, nextArrow, prevArrow;
 
-    var CONSTRUCTOR_ERROR = /LightBox constructor requires dom node and data model array/;
+    before(function(){
+        CONSTRUCTOR_ERROR = /LightBox constructor requires dom node and data model array/;
 
-    it('constructor should throw error if first arg is not a DOM node',function(){
-
-        var badConstructor = function() {
-            return new LightBox("#type-error", []);
-        };
-
-        var goodConstructor = function() {
+        goodConstructor = function() {
             return new LightBox(document.createElement('div'), []);
         };
 
-        var fnBad = function(){new badConstructor()};
-        var fnGood = function(){new goodConstructor()};
+        badConstructor = function() {
+            return new LightBox('#type-error', {});
+        };
 
+        fnBad = function(){new badConstructor()};
+        fnGood = function(){new goodConstructor()};
+    });
+
+    beforeEach(function(){
+        instance = new LightBox(document.createElement('div'), []);
+        closeBtn = instance.elm.querySelector('.close');
+        mainImg = instance.elm.querySelector('.main');
+        nextArrow = instance.elm.querySelector('.arrow-next');
+        prevArrow = instance.elm.querySelector('.arrow-prev');
+    });
+
+    it('constructor should throw error if first arg is not a DOM node',function(){
         expect(fnBad).to.throw(CONSTRUCTOR_ERROR);
         expect(fnGood).not.to.throw(CONSTRUCTOR_ERROR);
     });
 
     it('constructor should throw error if second arg is not an Array',function(){
-
-        var badConstructor = function() {
-            return new LightBox(document.createElement('div'), {});
-        };
-
-        var goodConstructor = function() {
-            return new LightBox(document.createElement('div'), []);
-        };
-
-        var fnBad = function(){new badConstructor()};
-        var fnGood = function(){new goodConstructor()};
-
         expect(fnBad).to.throw(CONSTRUCTOR_ERROR);
         expect(fnGood).not.to.throw(CONSTRUCTOR_ERROR);
     });
 
     it('should contain show, hide, prev, next, and update methods',function(){
-
-        var lightbox = new LightBox(document.createElement('div'), []);
-
-        expect(lightbox).to.have.property('show');
-        expect(lightbox).to.have.property('hide');
-        expect(lightbox).to.have.property('prev');
-        expect(lightbox).to.have.property('next');
-        expect(lightbox).to.have.property('update');
+        expect(instance).to.have.property('show');
+        expect(instance).to.have.property('hide');
+        expect(instance).to.have.property('prev');
+        expect(instance).to.have.property('next');
+        expect(instance).to.have.property('update');
     });
 
     it('template should have .lightbox, .main, .close, .arrow-prev, and .arrow-next selectors',function(){
-
-        var lightbox = new LightBox(document.createElement('div'), []);
-
-        var closeBtn = lightbox.elm.querySelector('.close');
-        var mainImg = lightbox.elm.querySelector('.main');
-        var nextArrow = lightbox.elm.querySelector('.arrow-next');
-        var prevArrow = lightbox.elm.querySelector('.arrow-prev');
-
-        expect(lightbox.elm.classList[0]).to.equal('lightbox');
+        expect(instance.elm.classList[0]).to.equal('lightbox');
         expect(closeBtn.classList[0]).to.equal('close');
         expect(mainImg.classList[0]).to.equal('main');
         expect(nextArrow.classList[0]).to.equal('arrow-next');
@@ -72,98 +58,71 @@ describe('LightBox instance', function(){
 });
 
 describe('LightBox behavior', function() {
-
     var LightBox = require('../app/js/gallery/lightbox');
-    var SHOW_ERROR = /Show method requires a string url and number key/;
+    var SHOW_ERROR, model, instance, showMethodGood, showMethodBad;
 
-    var model = [
-        "http://media.gettyimages.com/photos/from-style-to-success-picture-id514136469?s=170667a",
-        "http://media.gettyimages.com/photos/he-has-his-own-sense-of-style-picture-id478686645?s=170667a",
-        "http://media.gettyimages.com/photos/man-in-a-leather-jacket-leaning-on-a-brick-wall-picture-id499775043?s=170667a",
-        "https://c2.staticflickr.com/6/5574/30167563922_684e8d6796_b.jpg"
-    ];
+    before(function(){
+        SHOW_ERROR = /Show method requires a string url and number key/;
+
+        model = [
+            "http://media.gettyimages.com/photos/from-style-to-success-picture-id514136469?s=170667a",
+            "http://media.gettyimages.com/photos/he-has-his-own-sense-of-style-picture-id478686645?s=170667a",
+            "http://media.gettyimages.com/photos/man-in-a-leather-jacket-leaning-on-a-brick-wall-picture-id499775043?s=170667a",
+            "https://c2.staticflickr.com/6/5574/30167563922_684e8d6796_b.jpg"
+        ];
+    });
+
+    beforeEach(function(){
+        instance = new LightBox(document.createElement('div'), model);
+        showMethodGood = function(){instance.show(model[0], 0)};
+        showMethodBad = function(){instance.show(0, '')};
+    });
 
     it('show method should throw error if first argument is not a String', function(){
-
-        var lightbox = new LightBox(document.createElement('div'), model);
-
-        var fnBad = function(){lightbox.show(0, 0)};
-        var fnGood = function(){lightbox.show(model[0], 0)};
-
-        expect(fnBad).to.throw(SHOW_ERROR);
-        expect(fnGood).not.to.throw(SHOW_ERROR);
+        expect(showMethodGood).not.to.throw(SHOW_ERROR);
+        expect(showMethodBad).to.throw(SHOW_ERROR);
     });
 
     it('show method should throw error if second argument is not a Number', function(){
-
-        var lightbox = new LightBox(document.createElement('div'), model);
-
-        var fnBad = function(){lightbox.show(0, '0')};
-        var fnGood = function(){lightbox.show(model[0], 0)};
-
-        expect(fnBad).to.throw(SHOW_ERROR);
-        expect(fnGood).not.to.throw(SHOW_ERROR);
+        expect(showMethodGood).not.to.throw(SHOW_ERROR);
+        expect(showMethodBad).to.throw(SHOW_ERROR);
     });
 
     it('show method should add "show-lightbox" to lightbox elm classList', function(){
-
-        var lightbox = new LightBox(document.createElement('div'), model);
-
-        lightbox.show(model[0], 0);
-        expect(lightbox.elm.classList[1]).to.equal('show-lightbox');
+        instance.show(model[0], 0);
+        expect(instance.elm.classList[1]).to.equal('show-lightbox');
     });
 
     it('hide method should add "hide-lightbox" to lightbox elm classList', function(){
-
-        var lightbox = new LightBox(document.createElement('div'), []);
-
-        lightbox.show(model[0], 0);
-        lightbox.hide();
-
-        expect(lightbox.elm.classList[2]).to.equal('hide-lightbox');
+        instance.show(model[0], 0);
+        instance.hide();
+        expect(instance.elm.classList[2]).to.equal('hide-lightbox');
     });
 
     it('next method should increment index property', function(){
-
-        var lightbox = new LightBox(document.createElement('div'), model);
-        var nextIndex = lightbox.index + 1;
-
-        lightbox.show(model[0], 0);
-        lightbox.next();
-
-        expect(lightbox.index).to.equal(nextIndex);
+        var nextIndex = instance.index + 1;
+        instance.show(model[0], 0);
+        instance.next();
+        expect(instance.index).to.equal(nextIndex);
     });
 
     it('next method should reset index if at the end of list', function(){
-
-        var lightbox = new LightBox(document.createElement('div'), model);
         var i = model.length-1;
-
-        lightbox.show(model[i], i);
-        lightbox.next();
-
-        expect(lightbox.index).to.equal(0);
+        instance.show(model[i], i);
+        instance.next();
+        expect(instance.index).to.equal(0);
     });
 
     it('prev method should decrement index property', function(){
-
-        var lightbox = new LightBox(document.createElement('div'), model);
-
-        lightbox.show(model[1], 1);
-        lightbox.prev();
-
-        expect(lightbox.index).to.equal(0);
+        instance.show(model[1], 1);
+        instance.prev();
+        expect(instance.index).to.equal(0);
     });
 
     it('prev method should reset index if at the start of list', function(){
-
-        var lightbox = new LightBox(document.createElement('div'), model);
         var i = model.length-1;
-
-        lightbox.show(model[0], 0);
-        lightbox.prev();
-
-        expect(lightbox.index).to.equal(i);
+        instance.show(model[0], 0);
+        instance.prev();
+        expect(instance.index).to.equal(i);
     });
-
 });
