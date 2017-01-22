39dff2e8b216cbdfc9d283065c678fe4701aa74b
drobinso <drobinso@adobe.com>
2017-01-21 18:56:04 -0800

    temp disabling api call

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,30 @@
 
 var Gallery = require('./gallery/gallery');
 
-//Create the gallery when the DOM loads
+/**
+ * Create the gallery when the DOM loads
+ */
+
 document.addEventListener('DOMContentLoaded', function(e) {
 
+    let api = 'https://www.googleapis.com/customsearch/v1?imgSize=large&cx=016488772293305661751:46ntc6xv0k8&fileType=gif%2Cpng%2Cjpg%2Cjpeg&key=AIzaSyBBwGsQ13DP7SZEKfWekT8BTyuof6Edpcc';
 
     //Create some instances..
     let galleryA = new Gallery({
         node: document.querySelector('#gallery-a'),
-        autoload:false,
+        autoload:true,
         num: 10,
-        term: 'cats'//,
-        //uri: 'https://www.googleapis.com/customsearch/v1?imgSize=large&cx=016488772293305661751:46ntc6xv0k8&fileType=gif%2Cpng%2Cjpg%2Cjpeg&key=AIzaSyBBwGsQ13DP7SZEKfWekT8BTyuof6Edpcc'
+        term: 'cats',
+        uri: null
     });
 
-    // let galleryB = new Gallery({
-    //     node: document.querySelector('#gallery-b'),
-    //     num: 10,
-    //     term: 'snowboarding',
-    //     uri: 'https://www.googleapis.com/customsearch/v1?imgSize=large&cx=016488772293305661751:46ntc6xv0k8&fileType=gif%2Cpng%2Cjpg%2Cjpeg&key=AIzaSyBBwGsQ13DP7SZEKfWekT8BTyuof6Edpcc'
-    // });
+    let galleryB = new Gallery({
+        node: document.querySelector('#gallery-b'),
+        autoload:false,
+        num: 10,
+        term: 'snowboarding',
+        uri: null
+    });
 
 
 });
